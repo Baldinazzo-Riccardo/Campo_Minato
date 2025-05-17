777fa73f15922eb72c3c15810498d5c6ae7d079b
Tommaso Scudella <tommaso.scudella08@gmail.com>
2025-05-17 09:22:02 +0200

    aggiunta form 2 e 3 grafica fa cacare

---
+++ Form2.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Campo_Minato
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_gioca = new System.Windows.Forms.Button();
+            this.btn_howto = new System.Windows.Forms.Button();
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_gioca
+            // 
+            this.btn_gioca.Location = new System.Drawing.Point(256, 308);
+            this.btn_gioca.Name = "btn_gioca";
+            this.btn_gioca.Size = new System.Drawing.Size(75, 23);
+            this.btn_gioca.TabIndex = 0;
+            this.btn_gioca.Text = "GIOCA";
+            this.btn_gioca.UseVisualStyleBackColor = true;
+            // 
+            // btn_howto
+            // 
+            this.btn_howto.Location = new System.Drawing.Point(455, 308);
+            this.btn_howto.Name = "btn_howto";
+            this.btn_howto.Size = new System.Drawing.Size(114, 23);
+            this.btn_howto.TabIndex = 1;
+            this.btn_howto.Text = "HOW TO PLAY";
+            this.btn_howto.UseVisualStyleBackColor = true;
+            this.btn_howto.Click += new System.EventHandler(this.btn_howto_Click);
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.ForeColor = System.Drawing.Color.White;
+            this.lbl_title.Location = new System.Drawing.Point(105, 50);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(639, 46);
+            this.lbl_title.TabIndex = 2;
+            this.lbl_title.Text = "WELCOME TO CAMPO MINATO";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Campo_Minato.Properties.Resources.erba;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_title);
+            this.Controls.Add(this.btn_howto);
+            this.Controls.Add(this.btn_gioca);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_gioca;
+        private System.Windows.Forms.Button btn_howto;
+        private System.Windows.Forms.Label lbl_title;
+    }
+}
---
+++ Form3.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Campo_Minato
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.lbl_rules = new System.Windows.Forms.Label();
+            this.btn_chiudi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_rules
+            // 
+            this.lbl_rules.AutoSize = true;
+            this.lbl_rules.BackColor = System.Drawing.Color.White;
+            this.lbl_rules.Location = new System.Drawing.Point(107, 58);
+            this.lbl_rules.Name = "lbl_rules";
+            this.lbl_rules.Size = new System.Drawing.Size(619, 320);
+            this.lbl_rules.TabIndex = 1;
+            this.lbl_rules.Text = resources.GetString("lbl_rules.Text");
+            // 
+            // btn_chiudi
+            // 
+            this.btn_chiudi.Location = new System.Drawing.Point(377, 402);
+            this.btn_chiudi.Name = "btn_chiudi";
+            this.btn_chiudi.Size = new System.Drawing.Size(75, 23);
+            this.btn_chiudi.TabIndex = 2;
+            this.btn_chiudi.Text = "CHIUDI";
+            this.btn_chiudi.UseVisualStyleBackColor = true;
+            this.btn_chiudi.Click += new System.EventHandler(this.btn_chiudi_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_chiudi);
+            this.Controls.Add(this.lbl_rules);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_rules;
+        private System.Windows.Forms.Button btn_chiudi;
+    }
+}
