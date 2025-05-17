245360dd7569cd4efd68a75b18a19f0a57317525
Baldinazzo-Riccardo <12999474@itisrossi.vi.it>
2025-05-17 09:41:18 +0200

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Campo_Minato
+namespace Campo_Minato
 {
     partial class Form1
     {
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgv_scheda = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_scheda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_scheda
+            // 
+            this.dgv_scheda.AllowUserToAddRows = false;
+            this.dgv_scheda.AllowUserToDeleteRows = false;
+            this.dgv_scheda.AllowUserToResizeColumns = false;
+            this.dgv_scheda.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Transparent;
+            this.dgv_scheda.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_scheda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_scheda.ColumnHeadersVisible = false;
+            this.dgv_scheda.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.dgv_scheda.EnableHeadersVisualStyles = false;
+            this.dgv_scheda.Location = new System.Drawing.Point(309, 243);
+            this.dgv_scheda.MultiSelect = false;
+            this.dgv_scheda.Name = "dgv_scheda";
+            this.dgv_scheda.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_scheda.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv_scheda.RowHeadersVisible = false;
+            this.dgv_scheda.RowHeadersWidth = 62;
+            this.dgv_scheda.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgv_scheda.RowTemplate.Height = 28;
+            this.dgv_scheda.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgv_scheda.Size = new System.Drawing.Size(328, 300);
+            this.dgv_scheda.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(989, 581);
+            this.Controls.Add(this.dgv_scheda);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Campo Minato";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_scheda)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgv_scheda;
     }
 }
 
